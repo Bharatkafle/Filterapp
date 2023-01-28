474807a712dc19e978effbb73174c0325a1c647f
bharat <bharatkafle775@gmail.com>
2023-01-28 14:41:58 +0545

    filter app

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='section about-section'>
+          <h2 className='section-title'>about page</h2>
+          <p>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p>
+    </div>
+  )
+}
+
+export default About
